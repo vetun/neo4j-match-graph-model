7b2cd10ef9e53ba2fa1367636e4eb7caed2f6d99
Agustin <arbustin2001@yahoo.com.ar>
2017-06-06 22:11:04 -0300

    add neo4j to the Application

--- src/main/java/ar/com/research/tournament/Application.java
+++ src/main/java/ar/com/research/tournament/Application.java
@@ -1,16 +1,33 @@
 package ar.com.research.tournament;
 
+import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Created by Agustin Sequeira on 6/6/2017.
  * Spring boot configuration class
  */
 @SpringBootApplication
+@EnableNeo4jRepositories(basePackages = "ar.com.research.tournament.repository")
+@EnableTransactionManagement
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        return new SessionFactory("ar.com.research.tournament.domain");
+    }
+
+    @Bean
+    public Neo4jTransactionManager transactionManager() {
+        return new Neo4jTransactionManager(sessionFactory());
     }
 }
